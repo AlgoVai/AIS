f202fca46a7972918f618af5d9c30ae00b7a8312
AlgoVai <walclock113@gmail.com>
2026-03-21 01:31:30 +0600

    frontend developing

--- AIS/src/jsxf/Administration/Institutiton.jsx
+++ AIS/src/jsxf/Administration/Institutiton.jsx
@@ -1,10 +1,44 @@
+import { useState } from "react"
 
 
 function Institution(){
 
+    const [insData , setData]  = useState(
+        {
+            Name : " ",
+            Address : " " , 
+            url : " ",
+            IsActive : " ",
+        }
+    )
+
     return (
         <>
-        <h1 style={{color:"red"}}>Alim uddin high School</h1>
+       <div className="ui-border">
+        <form>
+            <div className="my-row">
+                <div className="my-col">
+                    <div className="from-group">
+                        <label>Name</label>
+                        <input type="text" placeholder="Name"></input>
+                    </div>
+                </div>
+                <div className="my-col">
+                    <div className="from-group">
+                        <label>Address</label>
+                        <input type="textarea"  placeholder="Address"/>
+                    </div>
+                </div>
+                <div className="my-col">
+                    <div className="from-group">
+                        <label>IsActive</label>
+                        <input type="checkbox" ></input>
+                    </div>
+                </div>
+            </div>
+            
+        </form>
+       </div>
         </>
     )
 }
